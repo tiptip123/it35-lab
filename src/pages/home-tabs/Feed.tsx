8e3ad0592e591a8fe7074069512e328df434286f
tiptip123 <josephbaguio327@gmail.com>
2025-02-17 07:38:46 +0800

    added /home/tabs/pages

---
+++ src/pages/home-tabs/Feed.tsx
@@ -0,0 +1,26 @@
+import { 
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  const Feed: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Feed</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        </IonContent>
+      </IonPage>
+    );
+  };
+  export default Feed;
