2f26b4ce91e72cc59d970d847d13b69012892695
tiptip123 <josephbaguio327@gmail.com>
2025-03-13 08:57:38 +0800

    Updates

--- assets/keyboard2-legacy-u_0xeWbS.js
+++ assets/keyboard2-legacy-u_0xeWbS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BCPiG8jv.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-B9cVoD7x.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
