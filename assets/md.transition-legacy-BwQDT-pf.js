d8c8792069efdac4457aeab7edf14d8b33117274
tiptip123 <josephbaguio327@gmail.com>
2025-04-07 08:28:34 +0800

    Updates

--- assets/md.transition-legacy-BwQDT-pf.js
+++ assets/md.transition-legacy-BwQDT-pf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DQ2CYC5Z.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-LkU4lpyr.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
