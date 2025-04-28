80c5c54e6361cca9c40f11204852aeed8e3a8fbc
tiptip123 <josephbaguio327@gmail.com>
2025-04-28 08:28:13 +0800

    Updates

--- assets/ios.transition-legacy-ByidM6Dn.js
+++ assets/ios.transition-legacy-ByidM6Dn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CjpT5CYU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-kl9TiPNJ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
