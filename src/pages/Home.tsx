052e2363f18d96a08e6d98a257412047cbcb94e8
tiptip123 <josephbaguio327@gmail.com>
2025-05-15 08:19:09 +0800

    change the label and icon for messages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,7 @@ import {
   IonToolbar
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { bookOutline, search, star, personCircle } from 'ionicons/icons';
+import { bookOutline, search, star, personCircle, chatbubbleOutline } from 'ionicons/icons';
 import { Route, Redirect } from 'react-router-dom';  // Ensure you're using react-router-dom
 import Favorites from './home-tabs/Favorites';
 import Feed from './home-tabs/Feed';
@@ -26,7 +26,7 @@ import UserProfile from './home-tabs/UserProfile';
 const Home: React.FC = () => {
   const tabs = [
     { name: 'Feed', tab: 'feed', url: '/it35-lab/app/home/feed', icon: bookOutline },
-    { name: 'Search', tab: 'search', url: '/it35-lab/app/home/search', icon: search },
+    { name: 'Messages', tab: 'search', url: '/it35-lab/app/home/search', icon: chatbubbleOutline },
     { name: 'Favorites', tab: 'favorites', url: '/it35-lab/app/home/favorites', icon: star },
     { name: 'Profile', tab: 'profile', url: '/it35-lab/app/home/profile', icon: personCircle },
   ];
