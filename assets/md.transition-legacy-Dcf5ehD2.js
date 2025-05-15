95739bffd5c7c43046d2922064b2a95dbdb908a0
tiptip123 <josephbaguio327@gmail.com>
2025-05-15 08:48:39 +0800

    Updates

--- assets/md.transition-legacy-Dcf5ehD2.js
+++ assets/md.transition-legacy-Dcf5ehD2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CJQSwMYk.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CxiOOAJV.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
