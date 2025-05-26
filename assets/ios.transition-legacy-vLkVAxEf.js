70a04e30a05756c9cb4f51192efedcede3ca7444
tiptip123 <josephbaguio327@gmail.com>
2025-05-26 19:59:37 +0800

    Updates

--- assets/ios.transition-legacy-vLkVAxEf.js
+++ assets/ios.transition-legacy-vLkVAxEf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bm4MR8Kq.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CFkMFFyG.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
