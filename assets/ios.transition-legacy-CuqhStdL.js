574b5a1f525e726fe0407cc0f4bfc58ff1d26e22
tiptip123 <josephbaguio327@gmail.com>
2025-05-28 05:13:42 +0800

    Updates

--- assets/ios.transition-legacy-CuqhStdL.js
+++ assets/ios.transition-legacy-CuqhStdL.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BPma0c-z.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BMkc8LlF.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
